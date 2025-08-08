ce4746c2ed4d305eec08b86c8d30d9901614d606
Eduardo Borsato Reinert <eduardoreinert19@gmail.com>
2025-08-08 17:04:01 -0300

    versao 3

--- recuperar_senha.php
+++ recuperar_senha.php
@@ -28,6 +28,26 @@
             //simula o envio do email (grava em txt)
             simularEnvioEmail($email,$senha_temporaria);
             echo "<script>alert('Uma senha temporaria foi gerada e enviada (simulação). Verifique o arquivo emails_simulados.txt'); window.location.href='login.php';</script>";
+        } else {
+            echo "<script>alert('E-mail não encontrado!');</script>";
         }
     }
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar senha</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h2>Recuperar senha</h2>
+    <form action="recuperar_senha.php" method="POST">
+        <label for="email">Digite o seu E-mail cadastrado</label>
+        <input type="email" id="email" name="email" required>
+
+        <button type="submit">Enviar a senha temporária</button>
+    </form>
+</body>
+</html>
