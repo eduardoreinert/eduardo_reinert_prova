cf37a692c798bf76300be94e7927f8d3ba8986f7
Eduardo Borsato Reinert <eduardoreinert19@gmail.com>
2025-08-11 15:54:21 -0300

    versao 4.1
    Introduces input fields for name, email, password, and profile selection to the user registration form. Also adds submit and reset buttons, and a link to return to the main page.

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -40,7 +40,27 @@
 <body>
     <h2>Cadastrar Usuário</h2>
     <form action="cadastro_usuario.php" method="POST">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required>
 
+        <label for="email">E-mail:</label>
+        <input type="email" id="email" name="eamil" required> 
+
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" required> 
+
+        <label for="id_perfil">Perfil:</label>
+        <select id="id_perfil" name="id_perfil">
+            <option value="1">Administrador</option>
+            <option value="2">Secretaria</option>  
+            <option value="3">Almoxerife</option>  
+            <option value="4">Cliente</option>  
+        </select>
+
+        <button type="submit">Salvar</button>
+        <button type="reset">Cancelar</button>
     </form>
+
+    <a href="principal.php">Voltar</a>
 </body>
 </html>
