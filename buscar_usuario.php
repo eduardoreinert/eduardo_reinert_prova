57b30a2ea4fe7c8e0665a4ab9df9ee4da8a6a1cb
Eduardo Borsato Reinert <eduardoreinert19@gmail.com>
2025-08-14 17:14:14 -0300

    versao 5
    Created alterar_usuario.php to allow admins to search and edit users. Fixed a typo and improved buscar_usuario.php by adding a search button, table formatting, action links for editing/deleting users, and better handling of search results.

--- buscar_usuario.php
+++ buscar_usuario.php
@@ -4,7 +4,7 @@
 
     //verifica se o usuario tem permissao de adm ou secretaria
     if($_SESSION['perfil'] !=1 && $_SESSION['perfil'] !=2){
-        echo "<acript>alert('Acesso negado!');window.location.href='principal.php';</script>";
+        echo "<script>alert('Acesso negado!');window.location.href='principal.php';</script>";
         exit();
     }
 
@@ -22,7 +22,7 @@
         } else {
             $sql="SELECT * FROM usuario WHERE nome LIKE :busca_nome ORDER BY nome ASC";
             $stmt=$pdo->prepare($sql);
-            $stmt->bindValue(':busca_nome',"%$busca%", PDO::PARAM_STR);
+            $stmt->bindValue(':busca_nome',"$busca%", PDO::PARAM_STR); //MUDAR AQUI PARA A ENTREGA (ja mudei)
         }
     } else {
         $sql="SELECT * FROM usuario ORDER BY nome ASC";
@@ -44,9 +44,10 @@
     <form action="buscar_usuario.php" method="POST">
         <label for="busca">Digite o ID ou Nome(opcional): </label>
         <input type="text" id="busca" name="busca">
+        <button type="submit">Pesquisar</button>
     </form>
     <?php if(!empty($usuarios)): ?>
-        <table>
+        <table border="1" align="center">
             <tr>
                 <th>ID</th>
                 <th>Nome</th>
@@ -60,7 +61,17 @@
                     <td><?=htmlspecialchars($usuario['nome'])?></td>
                     <td><?=htmlspecialchars($usuario['email'])?></td>
                     <td><?=htmlspecialchars($usuario['id_perfil'])?></td>
+                    <td>
+                        <a href="alterar_usuario.php?id=<?=htmlspecialchars($usuario['id_usuario'])?>">Alterar</a>
+                        <a href="excluir_usuario.php?id=<?=htmlspecialchars($usuario['id_usuario'])?>" onclick="return confirm('Tem certeza da exclusão?')">Excluir</a>
+                    </td>
                 </tr>
+            <?php endforeach; ?>
         </table>
+        <?php else: ?>
+            <p>Nenhum usuário encontrado.</p>
+        <?php endif; ?>
+        </br>
+        <a href="principal.php">Voltar</a>
 </body>
 </html>
