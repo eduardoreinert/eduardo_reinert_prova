6c9dcfd7e4c4945be0739a9885044aba3b2ce435
Eduardo Borsato Reinert <eduardoreinert19@gmail.com>
2025-08-21 17:01:14 -0300

    validacoes

--- validacoes.js
+++ validacoes.js
@@ -22,3 +22,20 @@ function validarFuncionario() {
 
     return true;
 }
+
+function mascara(o, f){
+    v_obj=o;
+    v_fun=f;
+    setTimeout("execmascara()", 1);
+  }
+  
+  function execmascara(){
+    v_obj.value=v_fun(v_obj.value);
+  }
+  
+  function telefone(v){
+    v=v.replace(/\D/g, "");
+    v=v.replace(/^(\d{2})(\d)/g, "($1) $2");
+    v=v.replace(/(\d)(\d{4})$/, "$1-$2");
+    return v;
+  }
