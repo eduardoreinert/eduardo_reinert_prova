69ff65162a84561076f540c85bf99258186df79e
Eduardo Borsato Reinert <eduardoreinert19@gmail.com>
2025-08-22 16:07:41 -0300

    validacoes
    funções para validação de campos. ex.:telefone

--- validacoes.js
+++ validacoes.js
@@ -1,17 +1,18 @@
 function validarFuncionario() {
-    let nome = document.getElementById("nome_funcionario").value;
-    let telefone = document.getElementById("telefone").value;
+    
+    let telefone = document.getElementById("telefone").value.trim();
     let email = document.getElementById("email").value;
+    let nome = document.getElementById("nome_funcionario").value;
 
     if (nome.length < 3) {
         alert("O nome do funcionário deve ter pelo menos 3 caracteres.");
         return false;
     }
 
-    let regexTelefone = /^[0-9]{10,11}$/;
-    if (!regexTelefone.test(telefone)) {
-        alert("Digite um telefone válido (10 ou 11 dígitos).");
-        return false;
+    if(telefone.length !== 15){
+        alert('Telefone inválido!');
+        event.preventDefault();
+        return;
     }
 
     let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -22,20 +23,3 @@ function validarFuncionario() {
 
     return true;
 }
-
-function mascara(o, f){
-    v_obj=o;
-    v_fun=f;
-    setTimeout("execmascara()", 1);
-  }
-  
-  function execmascara(){
-    v_obj.value=v_fun(v_obj.value);
-  }
-  
-  function telefone(v){
-    v=v.replace(/\D/g, "");
-    v=v.replace(/^(\d{2})(\d)/g, "($1) $2");
-    v=v.replace(/(\d)(\d{4})$/, "$1-$2");
-    return v;
-  }
